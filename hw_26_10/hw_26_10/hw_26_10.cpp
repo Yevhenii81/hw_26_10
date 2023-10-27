1940ad369af43fc4ac20b51b55ed0f4886bcf61d
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2023-10-27 17:01:43 +0300

    Add files via upload

---
+++ hw_26_10/hw_26_10/hw_26_10.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Task #1\n";
+    double num1;
+    double num2;
+    double sum;
+
+    cout << "Input first number: ";
+    cin >> num1;
+    cout << "Input second number: ";
+    cin >> num2;
+
+    double* pNum1 = &num1;
+    double* pNum2 = &num2;
+    double* pSum = &sum;
+
+    *pSum = *pNum1 + *pNum2;
+    cout << "Sum: " << *pSum << "\n\n";
+
+    cout << "Task #2\n";
+
+    double height;
+    double idealWeight;
+    double weightDifference;
+    double temp = 100.0;
+
+    cout << "Input your height: ";
+    cin >> height;
+    cout << "Input your ideal weight: ";
+    cin >> idealWeight;
+
+    double* pHeight = &height;
+    double* pIdealWeight = &idealWeight;
+
+    double** ptrHeight = &pHeight;
+    double** ptrIdealWeight = &pIdealWeight;
+
+    weightDifference = **ptrHeight - temp - **ptrIdealWeight;
+
+    cout << "For ideal weight yor need: ";
+    if (weightDifference < 0)
+    {
+        cout << "lose " << weightDifference << " kg\n";
+    }
+    else
+    {
+        cout << "gain " << weightDifference << " kg\n";
+    }
+
+}
